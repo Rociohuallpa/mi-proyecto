24afa7ae93c664f3975a0c10f7fc32e7671985db
sofia_huaman <huamanquispesofia1@gmail.com>
2024-11-28 09:54:46 -0500

    cambio del index desde sofia

--- CLASES/index.php
+++ CLASES/index.php
@@ -1,7 +1,8 @@
 <?php
-require_once("clases/figuras.php")
+require_once("clases/figuras.php");
 $cuadrado = new Cuadrado(5);
 echo "Area del cuadrado: " . $cuadrado->area() . "<br>";
 
 $circulo = new Circulo(5);
 echo "Area del círculo: " . $circulo->area() . "<br>";
+echo "sofia modifico el index:";
